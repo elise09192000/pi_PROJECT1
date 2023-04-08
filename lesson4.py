f8d1858f2c7424c220e40f5e1855b2e60f71676d
elise09192000 <elise09192000@gmail.com>
2023-04-08 15:05:43 +0800

    modify

--- lesson4.py
+++ lesson4.py
@@ -10,6 +10,7 @@ import requests
 
 from gpiozero import Button
 from signal import pause
+from gpiozero import RGBLED
 
 state = False
 
@@ -18,11 +19,31 @@ def user_press():
     state = not state
     if state == True:
         print("開燈")
+        led.color=(1,0,0)
     else:
         print("關燈")
+        led.color=(0,0,0)
 
 button = Button(18)
+led = RGBLED(red=17, green=27, blue=22)
 
 button.when_pressed = user_press
 
 pause()
+
+
+
+
+
+
+#from gpiozero import Button
+#from signal import pause
+
+#def say_hello():
+#    print("Hello!")
+
+#button = Button(18)
+
+#button.when_pressed = say_hello
+
+#pause()
