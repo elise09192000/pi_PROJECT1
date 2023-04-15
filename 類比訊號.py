4942591547ebb74776d3947874265fdecb72cdc4
elise09192000 <elise09192000@gmail.com>
2023-04-15 15:27:40 +0800

    modify

--- 類比訊號.py
+++ 類比訊號.py
@@ -8,5 +8,10 @@ mcp3008 = gpiozero.MCP3008(channel=7)
 
 while(True):
     lightValue = round(mcp3008.value*1000)
+    tmperature = round(mcp3008._temperature.value*3.3*100,)
     print(lightValue)
+    if lightValue < 40:
+        buzzer.on()
+    else:
+        buzzer.off()
     sleep(1)
