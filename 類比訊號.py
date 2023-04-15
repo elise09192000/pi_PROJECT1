8016dfc1f82958a74e236addce9996687f6063c7
elise09192000 <elise09192000@gmail.com>
2023-04-15 11:28:47 +0800

    modify

--- 類比訊號.py
+++ 類比訊號.py
@@ -1,5 +1,12 @@
 import gpiozero
-from signal import pause
+#from signal import pause
+#mcp3008 = gpiozero.MCP3008(channel=7)
+#print(mcp3008.value)
+#pause()
+from time import sleep
 mcp3008 = gpiozero.MCP3008(channel=7)
-print(mcp3008.value)
-pause()
+
+while(True):
+    lightValue = round(mcp3008.value*1000)
+    print(lightValue)
+    sleep(1)
